5b0d97296e514dea4cf00c2c747f2e5dcbbec3ba
Kaan Boke <kaanboke@gmail.com>
2022-06-10 16:18:44 +0000

    First commit to CI demo

---
+++ hello.py
@@ -0,0 +1,5 @@
+def mult(a, b):
+    return a * b
+
+
+print(f"This file give us multiplication of 4,5 --> {mult(4,5)}")
---
+++ test_hello.py
@@ -0,0 +1,4 @@
+from hello import mult
+
+def test_mult():
+    assert mult(3,6)==18
